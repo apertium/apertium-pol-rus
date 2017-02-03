ee3cddc0f1c5506db6cb83da0b2e7736a968de80
Мария Шеянова <masha.shejanova@gmail.com>
2017-02-03 12:13:17 +0000

    a couple of new functions

--- dev/clean_pol_rus.py
+++ dev/clean_pol_rus.py
@@ -10,22 +10,35 @@ dictionaries: bab.ls, pons, classes
 # implement the algorithm for cleaning
 
 
-import os
-from urllib import request
-from urllib import parse
 from lxml import etree
+from lxml import html
+from urllib import parse
+from urllib import request
+import os
+import string
+import subprocess
 
-DICNAME = '../apertium-pol-rus.pol-rus.dix'
 
-def get_all_pairs(tag):
-    with open(DICNAME, 'r') as f:
+POS = 'n'
+BIDIX = '../apertium-pol-rus.pol-rus.dix'
+RUSDIX = '../../apertium-rus/apertium-rus.rus.dix'
+BABLA = 'http://pl.bab.la/slownik/polski-rosyjski/'
+TAGS_BOUNDARY = {'n' : 3, 'adj' : 2}
+TAGS_LEMMAS = {'n' : '<n>.*<sg><nom>', 'adj' : '<adj>.*<sg><nom>',
+               'vblex' : '<vblex>.*<inf>'}
+
+
+def get_all_pairs():
+    '''takes POS tag needed, returns all lines with pairs of this POS'''
+    with open(BIDIX, 'r') as f:
         t = f.readlines()
     translations = [line for line in t if '<e><p><l>' in line
-                    and '<s n="' + tag in line]
+                    and '<s n="' + POS in line]
     return translations
 
 
 def lines_parsed(translations):
+    '''takes lines with translations, returns dict '''
     d_of_tr = {}
     for line in translations:
         tree = etree.fromstring(line)
@@ -47,47 +60,62 @@ def get_line_info(tree, n):
     return word, tags
 
 
-def homo_dealer(d):
+def check_homonimy(d):
     for key in d:
         if len(d[key]) > 2:
-            new_translations = get_better(key, d[key])
+            new_translations = correct(key, d[key])
 
 
-def get_better(sword, translations):
-    pass
+def correct(sword, translations):
+    '''returns a list of lines with pairs'''
+    lexeme = sword[0]
+    # pons_tr = from_pons(parse.quote(lexeme))
+    babla_tr = from_babla(parse.quote(lexeme.encode()))
+    if babla_tr:
+        return [lexeme + sword[1] + tr for tr in babla_tr]
 
 
 def from_pons(word):
-    prefix = 'http://en.pons.com/translate?'
+    prefix, postfix = 'http://en.pons.com/translate?q=', '&l=plru&in=&lf=pl'
 
 
-
-def from_babla(word, tags, dictionary):
-    link_noun = parse.quote(noun)
-    noun_page = request.urlopen('http://pl.bab.la/slownik/polski-rosyjski/' + link_noun).read().decode('utf-8')
-    translations = lxml.html.fromstring(noun_page).xpath('.//a[@class="result-link"]')
+def from_babla(word):
+    result = []
+    page = request.urlopen(BABLA + word).read().decode('utf-8')
+    translations = html.fromstring(page).xpath('.//ul[@class="sense-group-results"]')[0]
+    translations = [ch[0] for ch in translations.getchildren() if ch[0].tag == 'a']
+    translations = [tr.text for tr in translations if all_cyrillic(tr.text)]
+    print(word)
     for tr in translations:
-        print(tr.text)
-        if verifier(tr.text) is not None:
-            dictionary.write('<e><p><l>' + noun + tags + '</l><r>' 
-                + tr.text.replace(' ', '<b/>') + verifier(tr.text) + '</r></p></e>\n')
+        print(tr)
+        line = tags_getter(tr)
+        if line:
+            result.append(line)
         else:
-            dictionary.write('<e><p><l>' + noun + tags + '</l><r>' 
-                        + tr.text.replace(' ', '<b/>') + '<s n="n"/></r></p></e>\n')
+            print('didnt find ' + tr)
+    return result
+
+
+def all_cyrillic(s): return not set(string.ascii_letters).intersection(set(s))
 
 
 def from_classes():
     prefix = 'http://www.classes.ru/all-polish/dictionary-polish-russian-term-2.htm' # всё сложно
-    pass
+
+
+def tags_getter(rword):
+    ana = subprocess.getoutput('echo {0} | lt-proc -a '
+        '../../apertium-rus/rus.automorf.bin | tr "/" "\n" '
+        '| grep "{1}"'.format(rword, TAGS_LEMMAS[POS]))
+    ana = ana.split('<')[:TAGS_BOUNDARY[POS] + 1]
+    return '<s n="'.join(ana).replace('>', '">')
 
 
 def main():
-    translations = get_all_pairs('n')
+    translations = get_all_pairs()
     d = lines_parsed(translations)
     # print(sum([len(d[key]) for key in d]))
-    homo_dealer(d)
+    check_homonimy(d)
 
-main()
-
-# def det_pairs():
-#     os.system('cat apertium-pol-rus.pol-rus.dix | grep -P \'<e><p><l>[^<]+<s n="\' > \tmp\pairs')
+# main()
+print(from_babla('naprawa'))
