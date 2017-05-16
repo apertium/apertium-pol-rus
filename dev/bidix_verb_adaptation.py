db56f38ff810b640657120d270915cf20b3033f6
Мария Шеянова <masha.shejanova@gmail.com>
2017-05-16 19:52:29 +0000

    cleaned from some stuff

--- dev/bidix_verb_adaptation.py
+++ dev/bidix_verb_adaptation.py
@@ -19,32 +19,54 @@ def get_data(dixname, ent_sec):
 
 
 def verb_ents_bidix(pairs):
-	"""
-	Takes a tree with word entries, returns a tuple with verb entries,
-	verb entries translated as expressions ans a tree of entries cleaned
-	from any verb entries.
-	"""
-	print('len of entries: ' + str(len(pairs)))
-	expressions, verbents = [], []
-	for pair in pairs:
-		if pair.xpath('./p/r/s[@n="vblex"]'):
-			# verbs.append(pair[0][-1].text + '\n')
-			if pair[0][-1][0].tag == 'b':
-				expressions.append(pair)
-			else:
-				verbents.append(pair)
-			pairs.remove(pair)
-	print('verbents: ' + str(len(verbents)))
-	print('len of cleaned entries: ' + str(len(pairs)))
-	return expressions, verbents, pairs
+    """
+    Takes a tree with word entries, returns a tuple with verb entries,
+    verb entries translated as expressions ans a tree of entries cleaned
+    from any verb entries.
+    """
+    print('len of entries: ' + str(len(pairs)))
+    expressions, verbents = [], []
+    for pair in pairs:
+        if pair.xpath('./p/r/s[@n="vblex"]'):
+            if pair[0][-1][0].tag == 'b':
+                expressions.append(pair)
+            else:
+                verbents.append(pair)
+            pairs.remove(pair)
+    print('verbents: ' + str(len(verbents)))
+    print('len of cleaned entries: ' + str(len(pairs)))
+    return expressions, verbents, pairs
+
+
+def change_verbents(verbents):
+    f = open('rus_verbs', 'w')
+    new_verbents = []
+    for pair in verbents:
+        rusverb = pair[0][-1].text
+        f.write(rusverb + '\n')
+    f.close()
+    return verbents
+
+
+def write_new_bidix(bidix, cleaned_pairs, verbents, expressions):
+    """
+    Takes a bidix tree, a subtree with entries, a list with verb entries
+    and a list with expressions. Extends the dictionary with the two lists
+    and writes it to file. 
+    """
+    cleaned_pairs.extend(verbents),
+    cleaned_pairs.extend(expressions)
+    text = etree.tostring(bidix, encoding='utf-8').decode()
+    with open(BIDIX + '-new', 'w') as f:
+        f.write(text)
 
 
 def main():
-    monoverbs, monodix = get_data(RUSDIX, 'section[@id="gci"]')
+    # monoverbs, monodix = get_data(RUSDIX, 'section[@id="gci"]')
     pairs, bidix = get_data(BIDIX, 'section[@id="main"]')
     expressions, verbents, cleaned_pairs = verb_ents_bidix(pairs)
     verbents = change_verbents(verbents)
-    bidix = build_new_bidix(bidix, verbents, expressions)
+    bidix = write_new_bidix(bidix, cleaned_pairs, verbents, expressions)
 
 
 if __name__ == '__main__':
